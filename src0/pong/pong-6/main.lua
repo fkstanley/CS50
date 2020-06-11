b9dbf2e70f4a86a4205aa2de1ecd6869a7a9738e
Fiona Stanley <fionakathrynstanley@gmail.com>
2020-06-11 17:48:50 +0100

    Adds comments to pong-6

--- src0/pong/pong-6/main.lua
+++ src0/pong/pong-6/main.lua
@@ -208,5 +208,6 @@ function displayFPS()
     -- simple FPS display across all states
     love.graphics.setFont(smallFont)
     love.graphics.setColor(0, 255, 0, 255)
+    -- .. is lua's way of doing string concatenation
     love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), 10, 10)
 end
