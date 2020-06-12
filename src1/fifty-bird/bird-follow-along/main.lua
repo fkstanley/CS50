2c5b263b0ae76e5f56e778938a2ebdbb8bb9dc7b
Fiona Stanley <fionakathrynstanley@gmail.com>
2020-06-12 15:00:40 +0100

    Add image files and window width

---
+++ src1/fifty-bird/bird-follow-along/main.lua
@@ -0,0 +1,11 @@
+push = require 'push'
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+-- 16:9 resolution
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+
+local background = love.graphics.newImage('background.png')
+local ground = love.graphics.newImage('ground.png')
