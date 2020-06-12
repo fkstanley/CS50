716a8b7742722acb8880aa6bbb5400428e59e728
Fiona Stanley <fionakathrynstanley@gmail.com>
2020-06-12 15:07:54 +0100

    Apply background and ground
    Minor bug fixes

--- src1/fifty-bird/bird-follow-along/main.lua
+++ src1/fifty-bird/bird-follow-along/main.lua
@@ -16,7 +16,7 @@ function love.load()
 
     love.window.setTitle('Fifty Bird')
 
-    push::setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         vsync = true,
         fullscreen = false,
         resizable = true
@@ -24,5 +24,18 @@ function love.load()
 end
 
 function love.resize(w, h)
-    push::resize(w, h)
+    push:resize(w, h)
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+function love.draw()
+    push:start()
+    love.graphics.draw(background, 0, 0)
+    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 16)
+    push:finish()
 end
