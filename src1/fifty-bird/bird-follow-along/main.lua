3eb0d3de72d3756277f216bf4b655ec67887aaf3
Fiona Stanley <fionakathrynstanley@gmail.com>
2020-06-12 15:04:03 +0100

    Set up reszing of screen

--- src1/fifty-bird/bird-follow-along/main.lua
+++ src1/fifty-bird/bird-follow-along/main.lua
@@ -9,3 +9,20 @@ VIRTUAL_HEIGHT = 288
 
 local background = love.graphics.newImage('background.png')
 local ground = love.graphics.newImage('ground.png')
+
+function love.load()
+    -- No blurriness
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    love.window.setTitle('Fifty Bird')
+
+    push::setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        fullscreen = false,
+        resizable = true
+    })
+end
+
+function love.resize(w, h)
+    push::resize(w, h)
+end
