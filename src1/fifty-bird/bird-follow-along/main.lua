1b6fe5c1bc2bde2ce844cd68f1c3eed2a0a39d6a
Fiona Stanley <fionakathrynstanley@gmail.com>
2020-06-12 15:19:36 +0100

    Add comments

--- src1/fifty-bird/bird-follow-along/main.lua
+++ src1/fifty-bird/bird-follow-along/main.lua
@@ -10,12 +10,17 @@ VIRTUAL_HEIGHT = 288
 local background = love.graphics.newImage('background.png')
 local ground = love.graphics.newImage('ground.png')
 
+--[[
+    Sets up the initial window
+]]
 function love.load()
     -- No blurriness
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
+    -- Set the title for the game
     love.window.setTitle('Fifty Bird')
 
+    -- Set up the virtual window
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         vsync = true,
         fullscreen = false,
@@ -23,16 +28,27 @@ function love.load()
     })
 end
 
+--[[
+    Allows for resizing
+]]
 function love.resize(w, h)
     push:resize(w, h)
 end
 
+--[[
+    Detects user input
+]]
 function love.keypressed(key)
+    -- If we press escape then quit the game
     if key == 'escape' then
         love.event.quit()
     end
 end
 
+
+--[[
+    Draws our screen
+]]
 function love.draw()
     push:start()
     love.graphics.draw(background, 0, 0)
